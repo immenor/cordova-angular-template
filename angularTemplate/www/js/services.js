d8717efb72d8a8de81a0a90788de560d34a4c189
claytonmeador <clayton.meador@gmail.com>
2014-04-29 14:32:46 -0600

    test

---
+++ angularTemplate/www/js/services.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('template.services', []).
+    factory('portfolioContentService', function($log) {
+        return {
+            interfaceExamples:  {
+                promo: { name: 'flava', image: 'images/flava.png', large: null, larger: null, description: null},
+                canary: { name: 'moniker', image: 'images/moniker.jpg', large: null, larger: null, description: null},
+                analytics: { name: 'gap', image: 'images/gap.jpg', large: null, larger: null, description: null},
+                skuloop: { name: 'say again', image: 'images/sayagain.jpg', large: null, larger: null, description: null},
+                shopping: { name: 'solid bump', image: 'images/sbr.png', large: null, larger: null, description: null},
+                po2: { name: 'skuloop', image: 'images/skuloop.png', large: null, larger: null, description: null}
+            },
+            identityExamples: {
+                sayagain: { name: 'sayagain', image: 'images/flava.png', large: null, larger: null, description: null},
+                solidbump: { name: 'solidbump', image: 'images/flava.png', large: null, larger: null, description: null},
+                skuloop: { name: 'skuloop', image: 'images/flava.png', large: null, larger: null, description: null},
+                machocity: { name: 'machocity', image: 'images/flava.png', large: null, larger: null, description: null},
+                hoover: { name: 'hoover', image: 'images/flava.png', large: null, larger: null, description: null},
+                po2: { name: 'po2', image: 'images/flava.png', large: null, larger: null, description: null}
+            },
+            musicExamples: {
+                solidbump: { name: 'solidbump', image: 'images/flava.png', large: null, larger: null, description: null},
+                smashhit: { name: 'smashhit', image: 'images/flava.png', large: null, larger: null, description: null},
+                iris: { name: 'iris', image: 'images/flava.png', large: null, larger: null, description: null}
+            }
+    }
+})
